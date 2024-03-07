c36525de53302dde0d8a1b6e0ff0c5805f0e88da
Dmitriy <162015957+tretyakovid@users.noreply.github.com>
2024-03-07 15:55:35 +0300

    Create Comparison.jsx

---
+++ pages/Comparison.jsx
@@ -0,0 +1,18 @@
+const Comparison = () => {
+  return (
+    <section className="comparison">
+      <div className="wrapper comparison__wrapper">
+        <h1 className="comparison__heading heading heading">
+          Сравнение товаров
+        </h1>
+        <div className="comparison__content content-block">
+          <span className="comparison__not-found-text not-found-text">
+            Ничего не найдено
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Comparison;
