4eb96efed8719e5f4421efe2992c685663bedf2a
Dmitriy <162015957+tretyakovid@users.noreply.github.com>
2024-03-07 15:55:09 +0300

    Create Home.jsx

---
+++ pages/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../components/Banner/Banner";
+import Categories from "../components/Categories/Categories";
+import Computers from "../components/Computers/Computers";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Categories />
+      <Computers />
+    </>
+  );
+};
+
+export default Home;
