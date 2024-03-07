7790221798add5d7cbaade86beed67bcbb7bec76
Dmitriy <162015957+tretyakovid@users.noreply.github.com>
2024-03-07 15:55:21 +0300

    Create NotFound.jsx

---
+++ pages/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <section className="not-found">
+      <div className="wrapper not-found__wrapper">
+        <h1 className="not-found__heading heading">
+          Страница не найдела либо удалена
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
